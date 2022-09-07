fcc8d811062b08a65ed8b9ef9872abfad46006e6
mikeburgh <mike@recut.com>
2022-09-07 14:52:09 -0700

    Tweaking curl options

--- zsh-in-docker.sh
+++ zsh-in-docker.sh
@@ -125,7 +125,7 @@ cd /tmp
 
 # Install On-My-Zsh
 if [ ! -d $HOME/.oh-my-zsh ]; then
-    sh -c "$(curl https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 fi
 
 # Generate plugin list
